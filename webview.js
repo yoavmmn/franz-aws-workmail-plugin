af428dca23193e36ae6004100b9fa60d6f82c76a
Yoav Maman <yoavmmn@gmail.com>
2017-03-09 21:45:22 +0200

    Added indication of the number of unopened emails

--- webview.js
+++ webview.js
@@ -1,3 +1,33 @@
 module.exports = (Franz, options) => {
+  const reload = (EventType) => {
+    return new Promise((resolve, reject) => {
+      const btn = document.getElementsByClassName('giraffe-hierarchy-node-refresh')[0];
+      const EventObject = document.createEvent('Events');
+      EventObject.initEvent(EventType, true, false);
 
-}
+      if (btn.dispatchEvent(EventObject)) {
+        resolve();
+      } else {
+        reject();
+      }
+    });
+  };
+
+  const getUnread = () => {
+    reload('click').then((data) => {
+      const nodes = document.getElementsByClassName('giraffe-hierarchy-node-counter');
+      let counter = 0;
+
+      for	(var i = 0; i < nodes.length; i++) {
+        let node = parseInt(nodes[i].innerText);
+        counter += (node > 0) ? node : 0;
+      }
+
+      Franz.setBadge(counter);
+    });
+  };
+
+  if (!window.location.pathname.includes('auth')) {
+    window.setInterval(getUnread, 3000);
+  }
+};
